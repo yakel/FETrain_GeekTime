9a7d81154feeee2b01504a0bf764ca2e65bfc71e
liyake <liyake@zlcxdev.com>
2021-09-08 10:01:52 +0800

    week 02: copy server.js from week 01

---
+++ week_02/html_parser/server.js
@@ -0,0 +1,46 @@
+const http = require('http')
+
+const port = 8088
+
+const htmlContent = `<html maaa=a >
+<head>
+  <style>
+  body div #myid {
+    width: 100px;
+    background-color: #ff5000;
+  }
+  body div img {
+    width: 30px;
+    background-color: #ff1111;
+  }
+  </style>
+</head>
+<body>
+  <div>
+    <img id="myid" />
+    <img />
+  </div>
+</body>
+</html>
+`
+
+http
+  .createServer((request, response) => {
+    let body = []
+    request
+      .on('error', (err) => {
+        console.error(err)
+      })
+      .on('data', (chunk) => {
+        body.push(chunk)
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString()
+        console.log('body:', body)
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        response.end(htmlContent)
+      })
+  })
+  .listen(port)
+
+console.log('server started, listening', port)
