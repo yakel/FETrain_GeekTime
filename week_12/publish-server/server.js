11f7ad5bad8ace2e699573f73905bd2b9fff6506
liyake <liyake@zlcxdev.com>
2021-11-30 10:00:15 +0800

    week 12

--- week_12/publish-server/server.js
+++ week_12/publish-server/server.js
@@ -1,8 +1,19 @@
 const http = require('http')
+const fs = require('fs')
 
 http
   .createServer(function (request, response) {
-    console.log(request)
-    response.end('success')
+    console.log('headers:', request.headers)
+
+    const file = fs.createWriteStream('../server/public/index.html')
+
+    request.on('data', (chunk) => {
+      console.log(chunk.toString())
+      file.write(chunk)
+    })
+    request.on('end', (chunk) => {
+      file.end(chunk)
+      response.end('success')
+    })
   })
   .listen(3000)
--- week_12/publish-tool/publish.js
+++ week_12/publish-tool/publish.js
@@ -1,14 +1,27 @@
 const http = require('http')
+const fs = require('fs')
 
 const request = http.request(
   {
     hostname: '127.0.0.1',
     port: 3000,
-    // headers: {
-    //   'content-type': 'application/octea-stream',
-    // },
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/octet-stream',
+    },
   },
-  (response) => {}
+  (response) => {
+    console.log('response:', response)
+  }
 )
 
-request.end()
+const file = fs.createReadStream('./sample.html')
+
+file.on('data', (chunk) => {
+  request.write(chunk)
+  console.log(chunk.toString())
+})
+
+file.on('end', (chunk) => {
+  request.end(chunk)
+})
