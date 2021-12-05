6898f5114bb0aa234ce87fd661e38b3609c81020
yake <yake.abc@gmail.com>
2021-12-05 21:44:57 +0800

    publish

--- week_12/publish-server/server.js
+++ week_12/publish-server/server.js
@@ -1,19 +1,95 @@
 const http = require('http')
-const fs = require('fs')
+const https = require('https')
+const unzipper = require('unzipper')
+const querystring = require('querystring')
+
+// auth route
+function auth(request, response) {
+  let query = request.url.match(/^\/auth\?([\s\S]+)$/)[1]
+  query = querystring.parse(query)
+  getToken(query.code, function (info) {
+    console.log('token info:', info)
+    const href = `http://localhost:4000/?token=${info.access_token}`
+    const html = `<a href=${href}>Publish</a>`
+    response.write(html)
+    response.end()
+  })
+}
+
+function getToken(code, callback) {
+  const clientId = 'Iv1.cf3ff6db2854defa'
+  const clientSecret = '428e7f5f672da5421d426ed8b98b1fef044b94b9'
+  let path = '/login/oauth/access_token'
+  path += `?code=${code}`
+  path += `&client_id=${clientId}`
+  path += `&client_secret=${clientSecret}`
+  const request = https.request(
+    {
+      host: 'github.com',
+      port: 443,
+      path: path,
+      method: 'POST',
+    },
+    function (response) {
+      let body = ''
+      response.on('data', (chunk) => {
+        body += chunk.toString()
+      })
+      response.on('end', () => {
+        const info = querystring.parse(body)
+        callback(info)
+      })
+    }
+  )
+  request.end()
+}
+
+// publish route
+function publish(request, response) {
+  let query = request.url.match(/^\/auth\?([\s\S]+)$/)[1]
+  query = querystring.parse(query)
+  getUser(query.token, function (info) {
+    if (info.login === 'yakel') {
+      request.pipe(unzipper.Extract({ path: '../server/public/' }))
+      request.on('end', function () {
+        response.end('success')
+      })
+    }
+  })
+}
+
+function getUser(token, callback) {
+  const request = https.request(
+    {
+      host: 'api.github.com',
+      port: 443,
+      path: '/user',
+      headers: {
+        Authorization: `token ${token}`,
+        'User-Agent': 'toy-publish',
+      },
+    },
+    function (response) {
+      let body = ''
+      response.on('data', (chunk) => {
+        body += chunk.toString()
+      })
+      response.on('end', () => {
+        const info = querystring.parse(body)
+        callback(info)
+      })
+    }
+  )
+  request.end()
+}
 
 http
   .createServer(function (request, response) {
-    console.log('headers:', request.headers)
-
-    const file = fs.createWriteStream('../server/public/index.html')
-
-    request.on('data', (chunk) => {
-      console.log(chunk.toString())
-      file.write(chunk)
-    })
-    request.on('end', (chunk) => {
-      file.end(chunk)
-      response.end('success')
-    })
+    console.log(request.url)
+    if (request.url.match(/^\/auth\?/)) {
+      return auth(request, response)
+    } else if (request.url.match(/^\/publish\?/)) {
+      return publish(request, response)
+    }
   })
   .listen(3000)
--- week_12/publish-tool/publish.js
+++ week_12/publish-tool/publish.js
@@ -1,27 +1,41 @@
 const http = require('http')
-const fs = require('fs')
+const archiver = require('archiver')
+const child_process = require('child_process')
+const querystring = require('querystring')
 
-const request = http.request(
-  {
-    hostname: '127.0.0.1',
-    port: 3000,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/octet-stream',
+// open github auth page in browser
+const clientId = 'Iv1.cf3ff6db2854defa'
+const url = `https://github.com/login/oauth/authorize?client_id=${clientId}`
+const cmd = `open ${url}`
+child_process.exec(cmd)
+
+// create server, then get toekn
+http
+  .createServer(function (request, response) {
+    let query = request.url.match(/^\/\?([\s\S]+)$/)[1]
+    query = querystring.parse(query)
+    publish(token)
+  })
+  .listen(4000)
+
+function publish(token) {
+  const request = http.request(
+    {
+      hostname: '127.0.0.1',
+      port: 3000,
+      method: 'POST',
+      path: `/publish?token=${token}`,
+      headers: {
+        'Content-Type': 'application/octet-stream',
+      },
     },
-  },
-  (response) => {
-    console.log('response:', response)
-  }
-)
+    (response) => {
+      console.log('response:', response)
+    }
+  )
 
-const file = fs.createReadStream('./sample.html')
-
-file.on('data', (chunk) => {
-  request.write(chunk)
-  console.log(chunk.toString())
-})
-
-file.on('end', (chunk) => {
-  request.end(chunk)
-})
+  const archive = archiver('zip', { zlib: { level: 9 } })
+  archive.pipe(request)
+  archive.directory('./sample', false)
+  archive.finalize()
+}
